570bbe0728876f3bda0355c27a4e6fcf8db759cd
tamilmarant231-ux <tamilmarant231@gmail.com>
2026-02-11 15:16:45 +0530

    Add files via upload

---
+++ 6 if cont.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+int main() {
+    char ch;
+    scanf("%c",&ch);
+    if(ch=='a')
+    {
+        printf("vowel");
+    }
+    else if(ch=='e')     
+    {
+        printf("vowel");
+    }
+    else if(ch=='i')
+    {
+        printf("vowel");
+    }
+    else if(ch=='o')
+    {
+        printf("vowel");
+    }
+    else if(ch=='u')
+    {
+        printf("vowel");
+    }
+    else
+    {
+        printf("consonent");
+    }
+}
