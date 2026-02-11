a76dc03c40825135a568504e11e1701b49ba4043
tamilmarant231-ux <tamilmarant231@gmail.com>
2026-02-11 13:55:37 +0530

    Add files via upload

---
+++ 4 if cont.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main() {
+    int a,b;
+    scanf("%d %d",&a,&b);
+    if(b>a)
+    {
+        printf("20 is greater");
+    }
+    else
+    {
+        printf("false");
+    }
+    return 0;
+}
