3ffe9cd3868ed9e22abbcd70a8e49c527fb05600
tamilmarant231-ux <tamilmarant231@gmail.com>
2026-02-11 15:28:46 +0530

    Add files via upload

---
+++ 9 if cont.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main() {
+    int age;
+    scanf("%d",&age);
+    if(age>=18)
+    {
+        printf("eligible to vote");
+    }
+    else
+    {
+        printf("not eligible");
+    }
+}
