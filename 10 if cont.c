ea52d20d9bddf680e1269941b9a355b9b7f8a1f6
tamilmarant231-ux <tamilmarant231@gmail.com>
2026-02-11 15:40:49 +0530

    Add files via upload

---
+++ 10 if cont.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main() {
+    int a;
+    scanf("%d",&a);
+    if(a>=100&&a<=900)
+    {
+        printf("three-digit-number");
+    }
+    else
+    {
+        printf("else");
+    }
+}
